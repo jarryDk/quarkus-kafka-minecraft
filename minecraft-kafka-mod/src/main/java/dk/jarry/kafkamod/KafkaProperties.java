66e1f759ceab33d0912fc1896f6be3ec04d0fff9
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2022-08-12 09:49:16 +0200

    Readme - Kafka

--- minecraft-kafka-mod/src/main/java/dk/jarry/kafkamod/KafkaProperties.java
+++ minecraft-kafka-mod/src/main/java/dk/jarry/kafkamod/KafkaProperties.java
@@ -6,7 +6,7 @@ public class KafkaProperties {
     public final static String KAFKA_MOD_ITEM_STACK = "kafka-mod-item-stack";
     public final static String KAFKA_MOD_ENTITY_EVENT = "kafka-mod-entity-event";
     public final static String KAFKA_BOOTSTRAP_SERVERS = "localhost:9092";
-    
+
     private KafkaProperties(){}
 
 }
