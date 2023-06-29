7a09aebfef20b7486213b2b00bec322d1f5cdd5e
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2023-06-29 07:29:14 +0200

    Update to 1.20.1-47.0.19

--- minecraft-kafka-mod/deploy_light.sh
+++ minecraft-kafka-mod/deploy_light.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FORGE_SERVER_VERSION=${VERSION_MINECRAFT_FORGE_SERVER:=1.19.4-45.0.49}
+FORGE_SERVER_VERSION=${VERSION_MINECRAFT_FORGE_SERVER:=1.20.1-47.0.19}
 FORGE_SERVER_LOCATION="/opt/minecraft/forge/.minecraft_$FORGE_SERVER_VERSION"
 
 FORGE_MOD_FOLDER="$FORGE_SERVER_LOCATION/mods"
@@ -12,4 +12,4 @@ else
     echo "Forge folder was present -> $FORGE_MOD_FOLDER"
 fi
 
-cp -v build/libs/kafka-0.2.jar $FORGE_MOD_FOLDER/kafka-0.2.jar
+cp -v build/libs/kafka-0.3.jar $FORGE_MOD_FOLDER/kafka-0.3.jar
