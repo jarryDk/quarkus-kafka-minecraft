e32a7137f4c3f0cf8df6f2f438045fbbdc7ec54e
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2023-08-07 14:36:42 +0200

    minecraft-kafka-mod v1.0.0

--- minecraft-kafka-mod/deploy.sh
+++ minecraft-kafka-mod/deploy.sh
@@ -12,4 +12,4 @@ else
     echo "Forge folder was present -> $FORGE_MOD_FOLDER"
 fi
 
-cp -v build/libs/kafka-0.3.jar $FORGE_MOD_FOLDER/kafka-0.3.jar
+cp -v build/libs/kafka-1.0.0.jar $FORGE_MOD_FOLDER/kafka-1.0.0.jar
