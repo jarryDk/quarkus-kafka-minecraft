c5177b469edbab8bfdb27b1d15002abab927c6c4
Michael Bornholdt Nielsen <jarryDk@users.noreply.github.com>
2023-08-16 11:55:44 +0200

    Update create_kafka_ssl_volume.sh

--- containers/create_kafka_ssl_volume.sh
+++ containers/create_kafka_ssl_volume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 podman volume create minecraft-kafka_kafka-ssl
-podman import ../ssl/kafka-ssl.tar minecraft-kafka_kafka-ssl
+podman volume import minecraft-kafka_kafka-ssl ../ssl/kafka-ssl.tar
